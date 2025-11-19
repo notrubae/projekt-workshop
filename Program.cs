6d6a5e18873704fe740b4dd4b6f16c0e3d70c462
Ivan Rosancic <ivanrosancic@gmail.com>
2025-11-19 10:16:02 +0100

    w

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt_workshop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
